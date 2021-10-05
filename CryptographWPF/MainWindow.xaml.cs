a37106121909d8e06b4951b1fbff71f9bc12236d
YourAndrew101 <andrewratushnyak0@gmail.com>
2021-10-05 20:51:28 +0300

    Add SimpleKeyPanel

--- CryptographWPF/MainWindow.xaml.cs
+++ CryptographWPF/MainWindow.xaml.cs
@@ -31,6 +31,7 @@ namespace CryptographWPF
         private enum Acts { Crypto, Decrypto};
         private Acts _act;
 
+        private StackPanel _keyStackPanel = new StackPanel();
 
         public MainWindow()
         {
@@ -76,22 +77,37 @@ namespace CryptographWPF
         private void CryptoRadioButton_Checked(object sender, RoutedEventArgs e)
         {
             _act = Acts.Crypto;
+            _inputPlaceholder = "Текст для шифрування...";
+            _outputPlaceholder = "Зашифрований текст";
+
             UpdatePanels();
         }
         private void DecryptoRadioButton_Checked(object sender, RoutedEventArgs e)
         {
             _act = Acts.Decrypto;
+            _inputPlaceholder = "Текст для дешифрування...";
+            _outputPlaceholder = "Розшифрований текст";
+
             UpdatePanels();
         }
 
 
         private void UpdatePanels()
         {
-            if(_act == Acts.Crypto)
+            InputTextBox.Text = _inputPlaceholder;
+            OutputTextBox.Text = _outputPlaceholder;
+
+            MainGrid.Children.Remove(_keyStackPanel);
+
+            if (_act == Acts.Crypto)
             {
                 ActionButton.Content = "Шифрувати";
 
-                if (_encryptionType == _encryptionTypes[2] || _encryptionType == _encryptionTypes[7]) SetSimpleKeyPanel();
+                if (_encryptionType == _encryptionTypes[2] || _encryptionType == _encryptionTypes[7])
+                {
+                    GetSimpleKeyStackPanel();
+                    SetKeyStackPanel();
+                }
             }
             if(_act == Acts.Decrypto)
             {
@@ -99,13 +115,57 @@ namespace CryptographWPF
             }
         }
 
-        private void SetSimpleKeyPanel()
+        private void SetKeyStackPanel()
         {
-            var sp = new StackPanel();
-            sp.Children.Add(new TextBlock());
-            MainGrid.Children.Add(sp);
-            Grid.SetColumn(sp, 0);
-            Grid.SetRow(sp, 1);
+            MainGrid.Children.Add(_keyStackPanel);
+            Grid.SetColumn(_keyStackPanel, 2);
+            Grid.SetRow(_keyStackPanel, 4);
         }
+
+        private void GetSimpleKeyStackPanel()
+        {
+            _keyStackPanel.Children.Clear();
+
+            TextBox keyTextBox = new TextBox();
+            keyTextBox.Style = Application.Current.FindResource("SimpleKeyTextBoxStyle") as Style;
+            string keyTextBoxPlaceholder = "Ваш ключ...";
+            keyTextBox.Text = keyTextBoxPlaceholder;
+            keyTextBox.GotFocus += KeyTextBox_GotFocus;
+            keyTextBox.LostFocus += KeyTextBox_LostFocus;
+
+            void KeyTextBox_LostFocus(object sender, RoutedEventArgs e)
+            {
+                if (((TextBox)sender).Text == "" || ((TextBox)sender).Text == null) ((TextBox)sender).Text = keyTextBoxPlaceholder;
+            }
+            void KeyTextBox_GotFocus(object sender, RoutedEventArgs e)
+            {
+                if (((TextBox)sender).Text == keyTextBoxPlaceholder) ((TextBox)sender).Text = "";
+            }
+
+            Label label = new Label();
+            label.Style = Application.Current.FindResource("SimpleKeyLabelStyle") as Style;
+
+            TextBox keyLengthTextBox = new TextBox();
+            keyLengthTextBox.Style = Application.Current.FindResource("SimpleKeyTextBoxStyle") as Style; 
+            string keyLengthTextBoxPlaceholder = "Довжина ключа...";
+            keyLengthTextBox.Text = keyLengthTextBoxPlaceholder;
+            keyLengthTextBox.GotFocus += KeyLengthTextBox_GotFocus;
+            keyLengthTextBox.LostFocus += KeyLengthTextBox_LostFocus;
+
+            void KeyLengthTextBox_LostFocus(object sender, RoutedEventArgs e)
+            {
+                if (((TextBox)sender).Text == "" || ((TextBox)sender).Text == null) ((TextBox)sender).Text = keyLengthTextBoxPlaceholder;
+            }
+            void KeyLengthTextBox_GotFocus(object sender, RoutedEventArgs e)
+            {
+                if (((TextBox)sender).Text == keyLengthTextBoxPlaceholder) ((TextBox)sender).Text = "";
+            }
+
+
+            _keyStackPanel.Children.Add(keyTextBox);
+            _keyStackPanel.Children.Add(label);
+            _keyStackPanel.Children.Add(keyLengthTextBox);
+        }
+
     }
 }
