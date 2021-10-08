4d8931a1107f0a7d3f933e8f82cc84750bbde26e
YourAndrew101 <andrewratushnyak0@gmail.com>
2021-10-08 13:00:21 +0300

    Add Styles for side menu buttons

---
+++ CryptographWPF/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CryptographWPF
+{
+    public partial class MainWindow : Window
+    {
+        private string _inputPlaceholder = "Текст для шифрування...";
+        private string _outputPlaceholder = "Зашифрований текст";
+
+        private readonly string[] _encryptionTypes = {"ROT1", "ROT13", "Шифр Цезаря", "Транспозиція", "Двійковий код",
+            "Вісімковий код", "Шістнадцятковий код", "Шифр Віженера", "RSA шифрування", "AES шифрування"};
+        private string _encryptionType;
+
+        private enum Acts { Crypto, Decrypto};
+        private Acts _act;
+
+
+        private enum Pages {EncryptionPage, ShorthandPage};
+        private Pages _currentPage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeComponentsSettings();
+        }
+
+        private void InitializeComponentsSettings()
+        {
+            InputTextBox.Text = _inputPlaceholder;
+            OutputTextBox.Text = _outputPlaceholder;
+
+            CryptoRadioButton.IsChecked = true;
+
+            EncryptionsComboBox.SelectedIndex = 0;
+
+            SetPageSettings();
+        }
+        private void SetPageSettings()
+        {
+            _currentPage = Pages.EncryptionPage;
+            EncryptionPageButton.Style = Application.Current.TryFindResource("CurrentPageSideMenuButton") as Style;
+            ShorthandPageButton.Style = Application.Current.TryFindResource("NotCurrentPageSideMenuButton") as Style;
+        }
+
+        private void InputTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (((TextBox)sender).Text == _inputPlaceholder) ((TextBox)sender).Text = "";
+        }
+        private void InputTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (((TextBox)sender).Text == "" || ((TextBox)sender).Text == null) ((TextBox)sender).Text = _inputPlaceholder;
+        }
+
+
+        private void EncryptionsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+            _encryptionType = ((TextBlock)selectedItem.Content).Text;
+
+            UpdatePanels();
+        }
+
+        private void CryptoRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _act = Acts.Crypto;
+            _inputPlaceholder = "Текст для шифрування...";
+            _outputPlaceholder = "Зашифрований текст";
+
+            UpdatePanels();
+        }
+        private void DecryptoRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _act = Acts.Decrypto;
+            _inputPlaceholder = "Текст для дешифрування...";
+            _outputPlaceholder = "Розшифрований текст";
+
+            UpdatePanels();
+        }
+
+
+        private void UpdatePanels()
+        {
+            InputTextBox.Text = _inputPlaceholder;
+            OutputTextBox.Text = _outputPlaceholder;
+
+            RemovePanels();
+            ClearPanels();
+
+            if (_act == Acts.Crypto)
+            {
+                ActionButton.Content = "Шифрувати";
+
+                if (_encryptionType == _encryptionTypes[2] || _encryptionType == _encryptionTypes[7]) GetSimpleKeyCryptoStackPanel();
+                if (_encryptionType == _encryptionTypes[8]) GetPairKeyCryptoStackPanel();
+                if (_encryptionType == _encryptionTypes[9])
+                {
+                    GetSimpleKeyCryptoStackPanel();
+                    GetCryptoEncodingDockPanels();
+                }
+            }
+            if (_act == Acts.Decrypto)
+            {
+                ActionButton.Content = "Дешифрувати";
+
+                if (_encryptionType == _encryptionTypes[2] || _encryptionType == _encryptionTypes[7]) GetSimpleKeyDecryptoStackPanel();
+                if (_encryptionType == _encryptionTypes[8]) GetPairKeyDecryptoStackPanel();
+                if (_encryptionType == _encryptionTypes[9])
+                {
+                    GetSimpleKeyDecryptoStackPanel();
+                    GetCryptoEncodingDockPanels();
+                    SwapEncodingsDockPanels();
+                }
+
+            }
+
+            SetPanels();
+        }
+
+        private void EncryptionPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            _currentPage = Pages.EncryptionPage;
+            EncryptionPageButton.Style = Application.Current.TryFindResource("CurrentPageSideMenuButton") as Style;
+            ShorthandPageButton.Style = Application.Current.TryFindResource("NotCurrentPageSideMenuButton") as Style;
+        }
+
+        private void ShorthandPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            _currentPage = Pages.ShorthandPage;
+            EncryptionPageButton.Style = Application.Current.TryFindResource("NotCurrentPageSideMenuButton") as Style;
+            ShorthandPageButton.Style = Application.Current.TryFindResource("CurrentPageSideMenuButton") as Style;
+        }
+    }
+}
--- CryptographWPF/MainWindow/MainWindowDynamicComponents.cs
+++ CryptographWPF/MainWindow/MainWindowDynamicComponents.cs
@@ -18,111 +18,10 @@ namespace CryptographWPF
 {
     public partial class MainWindow : Window
     {
-        private string _inputPlaceholder = "Текст для шифрування...";
-        private string _outputPlaceholder = "Зашифрований текст";
-
-        private readonly string[] _encryptionTypes = {"ROT1", "ROT13", "Шифр Цезаря", "Транспозиція", "Двійковий код",
-            "Вісімковий код", "Шістнадцятковий код", "Шифр Віженера", "RSA шифрування", "AES шифрування"};
-        private string _encryptionType;
-
-        private enum Acts { Crypto, Decrypto};
-        private Acts _act;
-
         private StackPanel _keyStackPanel = new StackPanel { Name = "KeyStackPanel" };
         private DockPanel _encodingInDockPanel = new DockPanel { Name = "EncodingInDockPanel" };
         private DockPanel _encodingOutDockPanel = new DockPanel { Name = "EncodingOutDockPanel" };
-
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            InitializeComponentsSettings();
-        }
-
-        private void InitializeComponentsSettings()
-        {
-            InputTextBox.Text = _inputPlaceholder;
-            OutputTextBox.Text = _outputPlaceholder;
-
-            CryptoRadioButton.IsChecked = true;
-
-            EncryptionsComboBox.SelectedIndex = 0;
-        }
-
-        private void InputTextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (((TextBox)sender).Text == _inputPlaceholder) ((TextBox)sender).Text = "";
-        }
-        private void InputTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (((TextBox)sender).Text == "" || ((TextBox)sender).Text == null) ((TextBox)sender).Text = _inputPlaceholder;
-        }
-
-
-        private void EncryptionsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox comboBox = (ComboBox)sender;
-            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
-            _encryptionType = ((TextBlock)selectedItem.Content).Text;
-
-            UpdatePanels();
-        }
-
-        private void CryptoRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            _act = Acts.Crypto;
-            _inputPlaceholder = "Текст для шифрування...";
-            _outputPlaceholder = "Зашифрований текст";
-
-            UpdatePanels();
-        }
-        private void DecryptoRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            _act = Acts.Decrypto;
-            _inputPlaceholder = "Текст для дешифрування...";
-            _outputPlaceholder = "Розшифрований текст";
-
-            UpdatePanels();
-        }
-
-
-        private void UpdatePanels()
-        {
-            InputTextBox.Text = _inputPlaceholder;
-            OutputTextBox.Text = _outputPlaceholder;
-
-            RemovePanels();
-            ClearPanels();
-
-            if (_act == Acts.Crypto)
-            {
-                ActionButton.Content = "Шифрувати";
-
-                if (_encryptionType == _encryptionTypes[2] || _encryptionType == _encryptionTypes[7]) GetSimpleKeyCryptoStackPanel();
-                if (_encryptionType == _encryptionTypes[8]) GetPairKeyCryptoStackPanel();
-                if (_encryptionType == _encryptionTypes[9])
-                {
-                    GetSimpleKeyCryptoStackPanel();
-                    GetCryptoEncodingDockPanels();
-                }
-            }
-            if (_act == Acts.Decrypto)
-            {
-                ActionButton.Content = "Дешифрувати";
-
-                if (_encryptionType == _encryptionTypes[2] || _encryptionType == _encryptionTypes[7]) GetSimpleKeyDecryptoStackPanel();
-                if (_encryptionType == _encryptionTypes[8]) GetPairKeyDecryptoStackPanel();
-                if (_encryptionType == _encryptionTypes[9])
-                {
-                    GetSimpleKeyDecryptoStackPanel();
-                    GetCryptoEncodingDockPanels();
-                    SwapEncodingsDockPanels();
-                }
-
-            }
-
-            SetPanels();
-        }
+  
 
         private void SetPanels()
         {
@@ -168,8 +67,8 @@ namespace CryptographWPF
         }
         private void GetSimpleKeyTextBox(StackPanel keyStackPanel)
         {
-            TextBox keyTextBox = new TextBox 
-            { 
+            TextBox keyTextBox = new TextBox
+            {
                 Style = Application.Current.TryFindResource("SimpleKeyCryptoTextBoxStyle") as Style,
                 Name = "SimpleKeyTextBox"
             };
@@ -197,8 +96,8 @@ namespace CryptographWPF
         }
         private void GetKeyLengthTextBox(StackPanel keyStackPanel)
         {
-            TextBox keyLengthTextBox = new TextBox 
-            { 
+            TextBox keyLengthTextBox = new TextBox
+            {
                 Style = Application.Current.TryFindResource("SimpleKeyCryptoTextBoxStyle") as Style,
                 Name = "SimpleKeyLengthTextBox"
             };
@@ -224,7 +123,7 @@ namespace CryptographWPF
             _keyStackPanel.Children.Clear();
 
             TextBox keyTextBox = new TextBox
-            { 
+            {
                 Style = Application.Current.TryFindResource("SimpleKeyCryptoTextBoxStyle") as Style,
                 Name = "SimpleKeyTextBox"
             };
@@ -255,8 +154,8 @@ namespace CryptographWPF
         }
         private void GetGeneralKeyTextBox(StackPanel keyStackPanel)
         {
-            TextBox generalKeyTextBox = new TextBox 
-            { 
+            TextBox generalKeyTextBox = new TextBox
+            {
                 Style = Application.Current.TryFindResource("PairKeyCryptoTextBoxStyle") as Style,
                 Name = "GeneralKeyTextBox"
             };
