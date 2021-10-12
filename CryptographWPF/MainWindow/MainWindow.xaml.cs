5f93dfaa29056b3160bbffaac2bcea3d51cbd54b
YourAndrew101 <andrewratushnyak0@gmail.com>
2021-10-12 12:37:39 +0300

    Add image dock panel

--- CryptographWPF/MainWindow/MainWindow.xaml.cs
+++ CryptographWPF/MainWindow/MainWindow.xaml.cs
@@ -35,7 +35,7 @@ namespace CryptographWPF
         {
             InitializeComponent();
             InitializeWindowControlButtons();
-            InitializeFrame();
+            InitializePage();
         }
         
         private void InitializeWindowControlButtons()
@@ -44,11 +44,13 @@ namespace CryptographWPF
             CloseButton.Click += (s, e) => Close();
         }
 
-        private void InitializeFrame()
+        private void InitializePage()
         {
+            //TODO load settings from file
             ActionFrame.Content = new EncryptionPage();
-            SetEncryptionButtonCurrentPage();
+            _CurrentPage = Pages.EncryptionPage;
         }
+
 
         private void ChangePage()
         {
@@ -59,8 +61,7 @@ namespace CryptographWPF
             }
             else if(_CurrentPage == Pages.ShorthandPage)
             {
-                //TODO change page
-                ActionFrame.Content = new EncryptionPage();
+                ActionFrame.Content = new ShorthandPage();
                 SetShorthandButtonCurrentPage();
             }
         }
@@ -76,6 +77,7 @@ namespace CryptographWPF
             ShorthandPageButton.Style = Application.Current.TryFindResource("CurrentPageSideMenuButton") as Style;
         }
 
+        //TODO add labels to not understendable buttons
         private void EncryptionPageButton_Click(object sender, RoutedEventArgs e)
         {
             if (_CurrentPage != Pages.EncryptionPage) _CurrentPage = Pages.EncryptionPage;
@@ -83,102 +85,6 @@ namespace CryptographWPF
         private void ShorthandPageButton_Click(object sender, RoutedEventArgs e)
         {
             if (_CurrentPage != Pages.ShorthandPage) _CurrentPage = Pages.ShorthandPage;
-        }
-
-        /*private void InitializeComponentsSettings()
-        {
-            InputTextBox.Text = _inputPlaceholder;
-            OutputTextBox.Text = _outputPlaceholder;
-
-            CryptoRadioButton.IsChecked = true;
-
-            EncryptionsComboBox.SelectedIndex = 0;
-
-            SetPageSettings();
-        }
-        private void SetPageSettings()
-        {
-            _currentPage = Pages.EncryptionPage;
-            EncryptionPageButton.Style = Application.Current.TryFindResource("CurrentPageSideMenuButton") as Style;
-            ShorthandPageButton.Style = Application.Current.TryFindResource("NotCurrentPageSideMenuButton") as Style;
-        }
-        
-
-        private void InputTextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (((TextBox)sender).Text == _inputPlaceholder) ((TextBox)sender).Text = "";
-        }
-        private void InputTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (((TextBox)sender).Text == "" || ((TextBox)sender).Text == null) ((TextBox)sender).Text = _inputPlaceholder;
-        }
-
-
-        private void EncryptionsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox comboBox = (ComboBox)sender;
-            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
-            _encryptionType = ((TextBlock)selectedItem.Content).Text;
-
-            UpdatePanels();
-        }
-
-        private void CryptoRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            _act = Acts.Crypto;
-            _inputPlaceholder = "Текст для шифрування...";
-            _outputPlaceholder = "Зашифрований текст";
-
-            UpdatePanels();
-        }
-        private void DecryptoRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            _act = Acts.Decrypto;
-            _inputPlaceholder = "Текст для дешифрування...";
-            _outputPlaceholder = "Розшифрований текст";
-
-            UpdatePanels();
-        }
-
-
-        private void UpdatePanels()
-        {
-            InputTextBox.Text = _inputPlaceholder;
-            OutputTextBox.Text = _outputPlaceholder;
-
-            RemovePanels();
-            ClearPanels();
-
-            if (_act == Acts.Crypto)
-            {
-                ActionButton.Content = "Шифрувати";
-
-                if (_encryptionType == _encryptionTypes[2] || _encryptionType == _encryptionTypes[7]) GetSimpleKeyCryptoStackPanel();
-                if (_encryptionType == _encryptionTypes[8]) GetPairKeyCryptoStackPanel();
-                if (_encryptionType == _encryptionTypes[9])
-                {
-                    GetSimpleKeyCryptoStackPanel();
-                    GetCryptoEncodingDockPanels();
-                }
-            }
-            if (_act == Acts.Decrypto)
-            {
-                ActionButton.Content = "Дешифрувати";
-
-                if (_encryptionType == _encryptionTypes[2] || _encryptionType == _encryptionTypes[7]) GetSimpleKeyDecryptoStackPanel();
-                if (_encryptionType == _encryptionTypes[8]) GetPairKeyDecryptoStackPanel();
-                if (_encryptionType == _encryptionTypes[9])
-                {
-                    GetSimpleKeyDecryptoStackPanel();
-                    GetCryptoEncodingDockPanels();
-                    SwapEncodingsDockPanels();
-                }
-
-            }
-
-            SetPanels();
-        }
-
-        */
+        } 
     }
 }
---
+++ CryptographWPF/Pages/ShorthandPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CryptographWPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ShorthandPage.xaml
+    /// </summary>
+    public partial class ShorthandPage : Page
+    {
+        public ShorthandPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
